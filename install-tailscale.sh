f81edf8bc07523a14900a6e38bcf76a9e6bf36d5
Ashot Nazaryan <contact@hego.co>
2024-01-31 17:05:02 -0800

    Update install-tailscale.sh

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.22.0}
-TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
-wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
-cp -r tailscale tailscaled /render/
-
-mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
+wget -O - https://tailscale.com/install.sh | bash
