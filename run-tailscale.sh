68b3ea87142c64fd8d3f4efcb73fc9add0d2f1ff
Ashot Nazaryan <contact@hego.co>
2024-01-31 17:15:44 -0800

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 PID=$!
 
 ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES" --advertise-exit-node; do
+until tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES"; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
